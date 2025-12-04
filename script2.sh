06c85e6a8eb76aa7b35f6732b31338a6da5be117
zlkd4rk <jupesantezto@uide.edu.ec>
2025-12-04 13:23:16 +0000

    Ya me quiero ir a la casa 2

---
+++ script2.sh
@@ -0,0 +1,8 @@
+uname -a
+#uname: Imprime toda la información básica del sistema en una sola línea.
+lscpu 
+#infromación detalla de la CPU
+free -h 
+#mostraba la memoria RAM y de ls SWAP y la configuración -h es "Human Readable" siendo legible para nosotros 
+df -h 
+#Miestra el espacio del disco duro "Disk Free", nuevamente el -h para la comprensión humana. 
